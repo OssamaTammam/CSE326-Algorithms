080e8dbd6cc7a23ad4fbf2f078cdc20f54259a1e
Ossama Tammam <ossamatammam02@gmail.com>
2023-10-30 04:27:21 +0200

    made power function better

--- Labs/Lab 1/Power.py
+++ Labs/Lab 1/Power.py
@@ -1,4 +1,4 @@
-# O(n) == log y
+# O(N) == log y
 def power(x, y):
     if x == 0:
         return 0
@@ -13,19 +13,16 @@ def power(x, y):
 
 
 def betterPower(x, y):
-    if x == 0:
-        return 0
+    if y < 0:
+        return 1 / betterPower(x, y*-1)
     if y == 0:
         return 1
 
-    if y > 0:
-        buffer = betterPower(x, y//2)
-        if y % 2 == 0:
-            return buffer * buffer
-        else:
-            return buffer * buffer * x
+    buffer = betterPower(x, y//2)
+    if y % 2 == 0:
+        return buffer * buffer
     else:
-        return 1/betterPower(x, y*-1)
+        return buffer * buffer * x
 
 
 sol = betterPower(2, 3)
